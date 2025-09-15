3041035e3cfa28da0918bf89202a177f6f00a1fc
rexd4735 <81005714+rexd4735@users.noreply.github.com>
2025-09-15 21:34:41 +0530

    Add files via upload

---
+++ project/src/assets/data/data.js
@@ -0,0 +1,455 @@
+export const menulists = [
+    {
+      id: 1,
+      path: "/",
+      link: "home",
+    },
+    {
+      id: 2,
+      path: "/shop",
+      link: "Shop",
+    },
+    {
+      id: 3,
+      path: "/blog",
+      link: "Blog",
+    },
+    {
+      id: 4,
+      path: "/about",
+      link: "about",
+    },
+    {
+      id: 5,
+      path: "/services",
+      link: "Services",
+    },
+    {
+      id: 6,
+      path: "/contact",
+      link: "Contact",
+    },
+  ];
+  
+  export const herolist = [
+    {
+      id: 1,
+      title: "For the Occasion",
+      description:
+        "When every moment matters.",
+      image: "../images/hero/instax.png",
+      price: [
+        {
+          color: "red",
+          value: 135.78,
+        },
+        {
+          color: "yellow",
+          value: 185.27,
+        },
+        {
+          color: "green",
+          value: 125.62,
+        },
+      ],
+      color: [
+        { value: "red" },
+        { value: "yellow" },
+        {
+          value: "green",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Explore 4K Cameras",
+      description:
+        "Preview the visual as it's ",
+      image: "../images/hero/4k-cam.jpg",
+      price: [
+        {
+          color: "red",
+          value: 154.23,
+        },
+        {
+          color: "yellow",
+          value: 123.43,
+        },
+        {
+          color: "green",
+          value: 43.34,
+        },
+      ],
+      color: [
+        { value: "red" },
+        { value: "yellow" },
+        {
+          value: "green",
+        },
+      ],
+    },
+  ];
+  
+  export const productlists = [
+    {
+      id: 1,
+      title: "Canon EOS R5",
+      description:
+        "The EOS R5 is a hugely versatile camera which works well in a range of scenarios, from landscape photography through to event, portrait, action, and wildlife photography. It's also an incredibly capable video camera.",
+      images: [
+        { image: "../images/product/yay1.png" },
+        { image: "../images/product/yay4.png" },
+        { image: "../images/product/yay8.png" },
+      ],
+      discount: 10,
+      rating: 2.5,
+      featured: true,
+      price: [
+        { color: "red", value: 135.78 },
+        { color: "yellow", value: 185.27 },
+        { color: "green", value: 125.62 },
+      ],
+      color: [{ value: "red" }, { value: "yellow" }, { value: "green" }],
+    },
+    {
+      id: 2,
+      title: "Leica M6",
+      description:
+        "The legendary Leica M6 is an icon. Intuitive, compact, and discreet, it allows you to get up close to the action – and to real emotions. Since 1984, it has been the camera of choice for many of the world's best photographers, who have used it to create countless iconic images.",
+      images: [
+        { image: "../images/product/yay2.png" },
+        { image: "../images/product/yay4.png" },
+        { image: "../images/product/yay8.png" },
+      ],
+      discount: 15,
+      rating: 4.2,
+      featured: false,
+      price: [
+        { color: "blue", value: 99.99 },
+        { color: "white", value: 120.5 },
+        { color: "brown", value: 85.75 },
+      ],
+      color: [{ value: "blue" }, { value: "white" }, { value: "brown" }],
+    },
+    {
+      id: 3,
+      title: "Nikon Z fc",
+      description:
+        "The Z fc mirrorless camera features a classic, tactile design fused with modern Z series technology. Equipped with a flip out vlogger screen, this DX-format 4K UHD compact camera delivers big image quality for photos and videos. ",
+      images: [
+        { image: "../images/product/yay3.png" },
+        { image: "../images/product/yay8.png" },
+        { image: "../images/product/product1.1.png" },
+      ],
+      discount: 12,
+      rating: 4.5,
+      featured: true,
+      price: [
+        { color: "clear", value: 29.99 },
+        { color: "brown", value: 35.5 },
+        { color: "green", value: 27.75 },
+      ],
+      color: [{ value: "clear" }, { value: "brown" }, { value: "green" }],
+    },
+    {
+      id: 4,
+      title: "Instax Mini 12",
+      description:
+        "The super bubbly Fujifilm INSTAX MINI 12 instant camera is bursting with joy, creativity and color. Not only is this instant camera available in six super-cute and appealing tones – Pastel Blue, Mint Green, Blossom Pink, Lilac Purple, Clay White and Stormy Weather – it’s also fully loaded with easy-to-use modes and controls.",
+      images: [
+        { image: "../images/product/yay4.png" },
+        { image: "../images/product/yay3.png" },
+        { image: "../images/product/yay8.png" },
+      ],
+      discount: 18,
+      rating: 4.8,
+      featured: false,
+      price: [
+        { color: "natural", value: 19.99 },
+        { color: "dark brown", value: 22.5 },
+        { color: "light brown", value: 17.75 },
+      ],
+      color: [
+        { value: "natural" },
+        { value: "dark brown" },
+        { value: "light brown" },
+      ],
+    },
+    {
+      id: 5,
+      title: "Minolta MND30",
+      description:
+        "The Minolta MND30 is a simple and fun camera capable of producing high-resolution images and ultra clear 2.7K QHD video even in a compact package. This convenient and compact device can cover a variety of shooting situations with its crystal clear lens and 4x digital zoom.",
+      images: [
+        { image: "../images/product/yay5.png" },
+        { image: "../images/product/yay4.png" },
+        { image: "../images/product/yay8.png" },
+      ],
+      discount: 15,
+      rating: 4.0,
+      featured: false,
+      price: [
+        { color: "dark", value: 14.99 },
+        { color: "light", value: 18.5 },
+        { color: "white", value: 12.75 },
+      ],
+      color: [{ value: "dark" }, { value: "light" }, { value: "white" }],
+    },
+    {
+      id: 6,
+      title: "OM System OM-5",
+      description:
+        "The OM-5 is not here to be flashy, steal the show, be in the way of your experience or even try to be the sole reason for your adventure. It is rather like your favourite pair of boots that lead you onto trails unknown; that bag that always has what you need, or that friend that helps you see the world in a beautifully different way.",
+      images: [
+        { image: "../images/product/yay6.png" },
+        { image: "../images/product/yay8.png" },
+        { image: "../images/product/yay3.png" },
+      ],
+      discount: 20,
+      rating: 4.2,
+      featured: true,
+      price: [
+        { color: "natural", value: 9.99 },
+        { color: "brown", value: 12.5 },
+        { color: "black", value: 8.75 },
+      ],
+      color: [{ value: "natural" }, { value: "brown" }, { value: "black" }],
+    },
+    {
+      id: 7,
+      title: "Nikon D5600",
+      description:
+        "Nikon's Expeed image processor combined with an outstanding autofocus system and the ability to shoot at ISO 25,600, and an expanded ISO sensitivity of 6,400 in Night Landscape mode, ensures low-light scenes and difficult lighting situations are handled with ease making the D5600 a low-light powerhouse.",
+      images: [
+        { image: "../images/product/yay7.png" },
+        { image: "../images/product/yay4.png" },
+        { image: "../images/product/yay3.png" },
+      ],
+      discount: 15,
+      rating: 3.5,
+      featured: false,
+      price: [
+        { color: "blue", value: 24.99 },
+        { color: "green", value: 29.5 },
+        { color: "brown", value: 22.75 },
+      ],
+      color: [{ value: "blue" }, { value: "green" }, { value: "brown" }],
+    },
+    {
+      id: 8,
+      title: "Super EBC Lens",
+      description:
+        "Super Electron Beam Coating (Super EBC) is Fujifilm's proprietary multi-layer lens coating. It is designed to minimize flare and ghosting for maximum light transmission and image quality.",
+      images: [
+        {
+          image: "../images/product/yay8.png",
+        },
+        { image: "../images/product/yay1.png" },
+        { image: "../images/product/yay4.png" },
+      ],
+      discount: 25,
+      rating: 4.8,
+      featured: true,
+      price: [
+        { color: "black", value: 799.99 },
+        { color: "gray", value: 849.5 },
+        { color: "beige", value: 780.75 },
+      ],
+      color: [{ value: "black" }, { value: "gray" }, { value: "beige" }],
+    },
+  ];
+  
+  /* import { FaShippingFast } from "react-icons/fa";
+  import { MdOutlineMarkUnreadChatAlt } from "react-icons/md";
+  import { FaCircleDollarToSlot } from "react-icons/fa6";
+  
+  const additionalInfo = [
+    {
+      id: 1,
+      title: "FREE SHIPPING",
+      description: "Enjoy Free Shipping On All Orders - No Minimum Purchase Required.",
+      icon: <FaShippingFast size={50} />,
+    },
+    {
+      id: 2,
+      title: "24/7 SUPPORT",
+      description: "Our Team Is Available 24/7 To Help With Any Questions Or Concerns.",
+      icon: <MdOutlineMarkUnreadChatAlt size={50} />,
+    },
+    {
+      id: 3,
+      title: "MONEY BACK",
+      description: "We Offer A 100% Money-Back Guarantee For Your Satisfaction.",
+      icon: <FaCircleDollarToSlot size={50} />,
+    },
+  ];
+   */
+  export const promotionalInfo = [
+    {
+      id: 1,
+      title: "Poloroid",
+      description:
+        "Shop & shipment across the whole North America. Lorem Ipsum is simply dummy text of It’s easy to get lost in the world of lovely valley vapour around and the meridian sun strikes the upper surface.",
+      image: "../images/promotional/promcamera1.png",
+    },
+    {
+      id: 2,
+      title: "Security Camera",
+      description:
+        "Organic Skincare for Glowing Complexion. Lorem Ipsum is simply dummy text of It’s easy to get lost in the world of lovely valley vapour around and the meridian sun strikes the upper surface.",
+      image: "../images/promotional/promcamera2.png",
+    },
+  ];
+  
+  export const instagramPosts = [
+    {
+      id: 1,
+      image: "../images/posts/yeah1.jpg",
+      caption: "Caption for post 1",
+      likes: 150,
+      comments: 25,
+    },
+    {
+      id: 2,
+      image: "../images/posts/yeah2.jpg",
+      caption: "Caption for post 2",
+      likes: 200,
+      comments: 30,
+    },
+    {
+      id: 3,
+      image: "../images/posts/yeah3.jpg",
+      caption: "Caption for post 3",
+      likes: 180,
+      comments: 20,
+    },
+    {
+      id: 4,
+      image: "../images/posts/yeah4.jpg",
+      caption: "Caption for post 4",
+      likes: 250,
+      comments: 40,
+    },
+    {
+      id: 5,
+      image: "../images/posts/yeah5.jpg",
+      caption: "Caption for post 5",
+      likes: 300,
+      comments: 35,
+    },
+    {
+      id: 6,
+      image: "../images/posts/yeah6.jpg",
+      caption: "Caption for post 6",
+      likes: 190,
+      comments: 15,
+    },
+  ];
+  
+  export const pageLinks = [
+    {
+      category: "Normal",
+      pages: [
+        {
+          id: 1,
+          title: "Normal",
+          type: "Normal",
+        },
+        {
+          id: 2,
+          title: "Shop With Sidebar",
+          type: "Shop",
+        },
+        {
+          id: 3,
+          title: "Shop With Category",
+          type: "Shop",
+        },
+        {
+          id: 4,
+          title: "Shop Filters Top Bar",
+          type: "Shop",
+        },
+        {
+          id: 5,
+          title: "Shop Wide",
+          type: "Shop",
+        },
+        {
+          id: 6,
+          title: "My Account",
+          type: "Normal",
+        },
+      ],
+    },
+    {
+      category: "About",
+      pages: [
+        {
+          id: 7,
+          title: "About Us",
+          type: "Normal",
+        },
+        {
+          id: 8,
+          title: "Portfolio",
+          type: "Normal",
+        },
+        {
+          id: 9,
+          title: "FAQs",
+          type: "Normal",
+        },
+        {
+          id: 10,
+          title: "Contact Us",
+          type: "Normal",
+        },
+        {
+          id: 11,
+          title: "Our Team",
+          type: "Normal",
+        },
+        {
+          id: 12,
+          title: "Term & Conditions",
+          type: "Normal",
+        },
+      ],
+    },
+    {
+      category: "Blog",
+      pages: [
+        {
+          id: 13,
+          title: "Blog Dark",
+          type: "Blog",
+        },
+        {
+          id: 14,
+          title: "Blog Light",
+          type: "Blog",
+        },
+        {
+          id: 15,
+          title: "Blog Half Image",
+          type: "Blog",
+        },
+        {
+          id: 16,
+          title: "Blog Column",
+          type: "Blog",
+        },
+        {
+          id: 17,
+          title: "Blog Category",
+          type: "Blog",
+        },
+        {
+          id: 18,
+          title: "Blog Archive",
+          type: "Blog",
+        },
+      ],
+    },
+  ];
